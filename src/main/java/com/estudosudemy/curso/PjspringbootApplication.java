6bde5b58e41da7408ce3d99e4bb9bb3e2f3f90b0
Carlos Silva <carloos19@gmail.com>
2022-03-07 15:18:00 -0300

    create projeto java spring boot

---
+++ src/main/java/com/estudosudemy/curso/PjspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.estudosudemy.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjspringbootApplication.class, args);
+	}
+
+}
